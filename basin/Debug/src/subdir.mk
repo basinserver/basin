99e290cdfb8b922b03795a5be75ba3b9a6292bb7
JavaProphet <max.bruce12@gmail.com>
2016-12-30 00:54:17 -0800

    Edit makefile

--- basin/Debug/src/subdir.mk
+++ basin/Debug/src/subdir.mk
@@ -95,7 +95,7 @@ C_DEPS += \
 src/%.o: ../src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -std=gnu11 -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	$(CC) $(CFLAGS) -std=gnu11 -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
