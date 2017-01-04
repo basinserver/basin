6a2276025b199f3f82eb1ca25d395cbce1d72a76
JavaProphet <max.bruce12@gmail.com>
2017-01-03 21:20:00 -0800

    Merge

--- basin/src/anticheat.c
+++ basin/src/anticheat.c
@@ -8,8 +8,9 @@
 
 int ac_chat(struct player* player, char* msg) {
 	AC_BEGIN
+
 	for (char* p = msg; *p; p++) {
-		if (*p < 32 || *p == 127 || *p == '\u00A7') {
+		if (*p < 32 || *p == 127 || *p == 0xA7) {
 			kickPlayer(player, "Invalid chat message");
 			AC_END(1);
 		}
@@ -26,13 +27,13 @@ int ac_tick(struct player* player, int onground) {
 int ac_ticklook(struct player* player, float yaw, float pitch) {
 	AC_BEGIN
 	if (!isfinite(yaw) || !isfinite(pitch))
-		AC_END(1)
+	AC_END(1)
 	AC_END(0)
 }
 
 int ac_tickpos(struct player* player, double x, double y, double z) {
 	AC_BEGIN
 	if (!isfinite(x) || !isfinite(y) || !isfinite(z))
-		AC_END(1)
+	AC_END(1)
 	AC_END(0);
 }
